12f49983534e52126d043842922ad0e6a3521f50
FarzanGit <119040324+FarzanGit@users.noreply.github.com>
2022-11-26 21:45:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/FarzanGit/charlie /charlie
+  git clone https://FarzanGit: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@github.com/FarzanGit/jerry /jerry
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /charlie
+  git clone $UPSTREAM_REPO /jerry
 fi
-cd /charlie
+cd /jerry
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
