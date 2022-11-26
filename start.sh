72bb91329a11c374cd201889adeba89586d9d543
FarzanGit <119040324+FarzanGit@users.noreply.github.com>
2022-11-26 21:56:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://FarzanGit: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@github.com/FarzanGit/jerry /jerry
+  git clone https://github.com/FarzanGit/jerry /jerry
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /jerry
