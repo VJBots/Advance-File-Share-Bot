0d7d1d5850adfc630317d46d74270e593ed4562e
FarzanGit <119040324+FarzanGit@users.noreply.github.com>
2022-11-27 20:46:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # FSUB
