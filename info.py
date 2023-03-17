f53d6fec61d788378a512a341bc8751587dc3542
FarzanGit <119040324+FarzanGit@users.noreply.github.com>
2023-03-17 09:04:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jerry:bot@cluster0.qof3hys.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jerry:tgx@cluster0.qof3hys.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
