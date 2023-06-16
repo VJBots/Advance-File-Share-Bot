7f4333eab67856ecd6e44e26ce4c73c4c03e9e1a
TGthings <136629768+TGthings@users.noreply.github.com>
2023-06-16 08:25:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/FarzanGit/jerry /jerry
+  git clone https://github.com/TGthings/Jerry /Jerry
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /jerry
+  git clone $UPSTREAM_REPO /Jerry
 fi
-cd /jerry
+cd /Jerry
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
