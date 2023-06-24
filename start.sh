7d7eba55601e91ad394d15dae04fd63e99c399f6
Albert Einstein <moviezhood@gmail.com>
2023-06-24 19:34:44 +0530

    Done

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-rm -rf Jerry
-git clone https://github.com/TGthings/Jerry Jerry
-cd Jerry
-pip3 install -U -r requirements.txt
-echo "Bot Started...."
-python3 bot.py
+rm -rf boted;
+git clone https://github.com/TGthings/Jerry boted;
+cd boted;
+pip3 install -U -r requirements.txt;
+echo "Bot Started....";
+python3 bot.py;
