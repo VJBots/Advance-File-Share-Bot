11eeacc1c86f91c55ba812cb4bae217197338647
TGthings <136629768+TGthings@users.noreply.github.com>
2023-07-09 14:51:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 rm -rf boted;
-git clone https://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@github.com/TGthings/Jerry boted;
+git clone https://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@github.com/TGthings/Jerry boted;
 cd boted;
 pip3 install -U -r requirements.txt;
 echo "Bot Started....";
